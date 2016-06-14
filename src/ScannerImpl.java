2eab4eea54f1e7a866ebd88f4e262f4e106c043a
annabeljump <annabel.jump@gmail.com>
2016-06-15 00:19:13 +0100

    Starting instantiation on scanner class

--- src/ScannerImpl.java
+++ src/ScannerImpl.java
@@ -1,3 +1,8 @@
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+
 /**
  * Takes a directory and scans it for files
  * File names are sorted into an Array
@@ -7,11 +12,13 @@ public class ScannerImpl implements Scanner {
 
     private String[] fileArray;
     private String folderName;
+    private List<String> fileLocations;
 
 
     /**
      * Takes a String being the file path of the folder containing the files to be
      * checked for the desired sequence, scans the folder for all files within.
+     * All potential files are checked and subfolders and other non-files are weeded out.
      * Returns an Array with all of the file names in for further processing.
      * @param folderName, the String containing the path to the folder to be scanned
      * @return an Array of Strings containing the file names in the folder
@@ -19,7 +26,14 @@ public class ScannerImpl implements Scanner {
 
     @Override
     public String[] scanFolder(String folderName) {
-        return new String[0];
+        File directory = new File(folderName);
+        File[] files = directory.listFiles();
+        for (int i = 0; i < files.length; i++) {
+            if (files[i].isFile()) {
+
+            }
+        }
+        return fileArray;
     }
 
     public ScannerImpl(String directoryName) {
