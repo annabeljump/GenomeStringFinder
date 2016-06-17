186c123f74f3e6c997ac1cd38d8019de49023e93
annabeljump <annabel.jump@gmail.com>
2016-06-17 23:21:29 +0100

    Finished interactive features

--- src/Main.java
+++ src/Main.java
@@ -9,17 +9,20 @@ public class Main {
 
     public static void main(String[] args) {
 
-        //TODO accept keyboard input and make interactive output
 
         System.out.println("Welcome to the Genome String Finder!");
+        System.out.println("");
         System.out.println("This tool will search through the files in a given directory");
         System.out.println("and return files that contain a match to the string");
         System.out.println("that you instruct it to find!");
+        System.out.println("");
         System.out.println("____________________________________________________________");
+        System.out.println("");
         System.out.println("First, please enter the path to the folder that the files you");
         System.out.println("wish to be searched are in (NB this should be in the format");
         System.out.println("/Users/you/folder/searchfolder or similar if you are on Mac, or");
         System.out.println("on Windows, this should be something like c:\\User\\folder\\etc.)");
+        System.out.println("");
         System.out.println("Please enter the folder path now:");
 
         String pathName = System.console().readLine();
@@ -35,7 +38,12 @@ public class Main {
         ArrayList<String> scanned = new ArrayList<>();
         scanned = sc.scanFolder();
 
-        Finder findME = new Finder("abcd");
+        System.out.println("Please enter the target sequence you wish to search for:");
+
+        String target = System.console().readLine();
+        Finder findME = new Finder(target);
+
+        System.out.println("Your sequence was matched in:");
 
         try {
             System.out.println(findME.findMatches(scanned));
